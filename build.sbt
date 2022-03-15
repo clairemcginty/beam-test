1a84891a8e849d8ca1f43b0f2d447fd02cd87762
Claire McGinty <clairem@spotify.com>
2022-03-15 11:08:25 -0400

    first commit

---
+++ build.sbt
@@ -0,0 +1,53 @@
+import sbt._
+import Keys._
+val scioVersion = "0.11.4"
+val beamVersion = "2.35.0"
+lazy val commonSettings = Def.settings(
+  organization := "clairem.data",
+  // Semantic versioning http://semver.org/
+  version := "0.1.0-SNAPSHOT",
+  scalaVersion := "2.13.8",
+  resolvers += "confluent" at "https://packages.confluent.io/maven/",
+  scalacOptions ++= Seq("-target:jvm-1.8",
+                        "-deprecation",
+                        "-feature",
+                        "-unchecked",
+                        "-Ymacro-annotations",
+                        ),
+  javacOptions ++= Seq("-source", "1.8", "-target", "1.8")
+)
+
+lazy val root: Project = project
+  .in(file("."))
+  .settings(commonSettings)
+  .settings(
+    name := "beam-test",
+    description := "beam-test",
+    publish / skip := true,
+    run / classLoaderLayeringStrategy := ClassLoaderLayeringStrategy.Flat,
+    run / fork := true,
+    libraryDependencies ++= Seq(
+      "com.spotify" %% "scio-core" % scioVersion,
+      "com.spotify" %% "scio-extra" % scioVersion,
+      "com.spotify" %% "scio-test" % scioVersion % Test,
+      "org.apache.beam" % "beam-runners-direct-java" % beamVersion,
+      "org.apache.beam" % "beam-runners-google-cloud-dataflow-java" % beamVersion,
+      "org.slf4j" % "slf4j-simple" % "1.7.36"
+    )
+  )
+  .enablePlugins(JavaAppPackaging)
+
+lazy val repl: Project = project
+  .in(file(".repl"))
+  .settings(commonSettings)
+  .settings(
+    name := "repl",
+    description := "Scio REPL for beam-test",
+    libraryDependencies ++= Seq(
+      "com.spotify" %% "scio-repl" % scioVersion
+    ),
+    Compile / mainClass := Some("com.spotify.scio.repl.ScioShell"),
+    publish / skip := true
+  )
+  .dependsOn(root)
+
---
+++ src/main/scala/clairem/data/JoinTest.scala
@@ -0,0 +1,80 @@
+package clairem.data
+
+import com.spotify.scio._
+import com.spotify.scio.extra.json._
+import com.spotify.scio.options.ScioOptions
+import org.apache.beam.sdk.coders.{KvCoder, StringUtf8Coder}
+import org.apache.beam.sdk.transforms.DoFn.ProcessElement
+import org.apache.beam.sdk.transforms.join.{CoGbkResult, CoGroupByKey, KeyedPCollectionTuple}
+import org.apache.beam.sdk.transforms.{DoFn, ParDo}
+import org.apache.beam.sdk.values.{KV, PCollection}
+
+/*
+To run:
+
+sbt "runMain clairem.data.JoinTest --runner=DataflowRunner --region=us-central1 --project=[[PROJECT]]"
+*/
+object JoinTest {
+
+  case class Edit(title: String, id: String, timestamp: Long, num_characters: Long)
+
+  def main(cmdlineArgs: Array[String]): Unit = {
+    val (sc, args) = ContextAndArgs(cmdlineArgs)
+
+    args.getOrElse("impl", "beam") match {
+      case "scio" => scioImpl(sc)
+      case "beam" => beamImpl(sc)
+      case _: String => ???
+    }
+    sc.run()
+  }
+
+  def beamImpl(sc: ScioContext): Unit = {
+    // Contrive data to get large key groups
+    val lhs: PCollection[KV[String, String]] = sc
+      .jsonFile[Edit]("gs://dataflow-samples/wikipedia_edits/wiki_data-0000000001*")
+      .map { edit => KV.of((edit.num_characters % 1000).toString, edit.id) }
+      .internal // to PCollection
+      .setCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of()))
+
+    val rhs: PCollection[KV[String, String]] = sc
+      .jsonFile[Edit]("gs://dataflow-samples/wikipedia_edits/*")
+      .map { edit => KV.of((edit.timestamp % 1000).toString, edit.title) }
+      .internal // to PCollection
+      .setCoder(KvCoder.of(StringUtf8Coder.of(), StringUtf8Coder.of()))
+
+    KeyedPCollectionTuple
+      .of[String, String]("lhs", lhs)
+      .and[String]("rhs", rhs)
+      .apply(CoGroupByKey.create[String])
+      .apply(ParDo.of(new DoFn[KV[String, CoGbkResult], String] {
+        @ProcessElement
+        private[data] def processElement(
+          c: DoFn[KV[String, CoGbkResult], String]#ProcessContext
+        ): Unit = {
+          val input = c.element()
+          val rhsIterable = input.getValue.getAll[String]("rhs")
+          val lhsIterable = input.getValue.getAll[String]("lhs")
+          rhsIterable.iterator().forEachRemaining { r =>
+            lhsIterable.iterator().forEachRemaining(l => {
+              c.output(s"$l-$r")
+            })
+          }
+        }
+    }))
+  }
+
+  def scioImpl(sc: ScioContext): Unit = {
+    val rhs = sc
+      .jsonFile[Edit]("gs://dataflow-samples/wikipedia_edits/*")
+      .map { edit => ((edit.timestamp % 250).toString, edit.title) }
+
+    val lhs = sc
+      .jsonFile[Edit]("gs://dataflow-samples/wikipedia_edits/wiki_data-00000000001*")
+      .map { edit => ((edit.num_characters % 250).toString, edit.id) }
+
+    lhs.join(rhs).map { case (k, (l, r)) =>
+      s"$l-$r"
+    }
+  }
+}
